2bd573a9568a0cf998b88744563f66555010eaa7
Jackson Tian <shyvo1987@gmail.com>
2025-05-13 21:01:48 +0800

    init project

---
+++ index.js
@@ -0,0 +1,18 @@
+import path from 'path';
+import { readFile } from 'fs/promises';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const favicon = await readFile(path.join(__dirname, 'assets/favicon.ico'));
+
+export default async function (ctx, next) {
+  if (ctx.method === 'GET' && ctx.path === '/favicon.ico') {
+    ctx.type = 'image/x-icon';
+    ctx.body = favicon;
+    return;
+  }
+
+  await next();
+}
